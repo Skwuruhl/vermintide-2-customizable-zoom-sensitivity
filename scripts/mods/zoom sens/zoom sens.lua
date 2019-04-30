98c8cc648bcc0a54df48b21300a683974796d516
Skwuruhl <smmarion11@gmail.com>
2019-04-30 03:31:13 -0700

    moved functions to table

--- scripts/mods/zoom sens/zoom sens.lua
+++ scripts/mods/zoom sens/zoom sens.lua
@@ -12,19 +12,34 @@ local coef = 1.0
 --]]
 
 -- "Private" function - not accessible to other mods
-local function update_sensMult()
-	local configFOV = Application.user_setting("render_settings", "fov") * math.pi / 180 --gets configured FOV in radians
-	if mode == "zoom_ratio" then
-		sensMult = configFOV / math.tan(configFOV/2) / 0.785 --calculates a sensitivity multiplier to make hipfire the same as without the mod.
+local function update_mode()
+	if mod:get("scaling_method") == "zoom_ratio" then
+		mode = 1
 	else
-		sensMult = configFOV / math.atan(coef*math.tan(configFOV/2)) / 0.785
+		mode = 2
 	end
+	coef = mod:get("coefficient") / 100.0
 end
 
-local function update_mode()
-	mode = mod:get("scaling_method")
-	coef = mod:get("coefficient") / 100
-end
+local update_sensMult = {
+	function()
+		local configFOV = Application.user_setting("render_settings", "fov") * math.pi / 180 -- gets configured FOV in radians
+		sensMult = configFOV / 0.785 / math.tan(configFOV/2) -- calculates a sensitivity multiplier to make starting (1x) zoom the same as without the mod.
+	end,
+	function()
+		local configFOV = Application.user_setting("render_settings", "fov") * math.pi / 180
+		sensMult = configFOV / 0.785 / math.atan(coef*math.tan(configFOV/2)) -- same thing but for the 2nd scaling method
+	end
+}
+
+local scalingMethod = { -- scalingMethod[1] is for zoom ratio, [2] for monitor distance
+	function(fov)
+		return math.tan(fov/2) * coef * sensMult
+	end,
+	function(fov)
+		return math.atan(coef*math.tan(fov/2)) * sensMult
+	end
+}
 
 -- "Public" function - accessible to other mods
 --function mod.my_function()
@@ -47,14 +62,10 @@ mod:hook_origin(CharacterStateHelper, "look", function (input_extension, viewpor
 	local is_3p = false
 	local look_delta = CharacterStateHelper.get_look_input(input_extension, status_extension, inventory_extension, is_3p)
 	
-	if mode == "zoom_ratio" then --replaced "look_delta = look_delta * look_sensitivity" with this.
-		if status_extension:is_zooming() then --only apply coefficient if zooming
-			look_delta = look_delta * math.tan(fieldOfView/2) * sensMult * coef
-		else
-			look_delta = look_delta * math.tan(fieldOfView/2) * sensMult --makes sensitivity scale by the tangent of FOV instead of just by FOV. Again, sensMult is to offset the change in hipfire sensitivity so it's the same as vanilla.
-		end
+	if status_extension:is_zooming() then -- only apply new scaling in ADS
+		look_delta = look_delta * scalingMethod[mode](fieldOfView)
 	else
-		look_delta = look_delta * math.atan(coef*math.tan(fieldOfView/2)) * sensMult --makes sensivivity scale by a different FOV aspect ratio. E.g. 177.78% coefficient would be scaling by horizontal FOV since 16/9 is 1.78. Or 133.33% works like CS:GO etc.
+		look_delta = look_delta * fieldOfView / 0.785 -- default scaling
 	end
 
 	if override_delta then
@@ -89,14 +100,14 @@ end ]]
 -- state  - "StateLoading", "StateIngame" etc.
 mod.on_game_state_changed = function(status, state)
 	update_mode()
-	update_sensMult()
+	update_sensMult[mode]()
 end
 
 -- Called when a setting is changed in mod settings
 -- Use mod:get(setting_name) to get the changed value
 mod.on_setting_changed = function(setting_name)
 	update_mode()
-	update_sensMult()
+	update_sensMult[mode]()
 end
 
 -- Called when the checkbox for this mod is unchecked
